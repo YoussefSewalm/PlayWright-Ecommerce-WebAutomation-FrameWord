7d6e2ced1b4be6b51b122452cbad94103cbb4b2a
YoussefSewalm <eng.yousefsewalem@gmail.com>
2025-01-12 01:16:25 +0200

    Update L1_AccountPage.java

--- src/main/java/Locators/L1_AccountPage.java
+++ src/main/java/Locators/L1_AccountPage.java
@@ -6,22 +6,14 @@ public class L1_AccountPage {
 	public static String VerifyAccountLoginPageText = "div[class='login-form']>h2";
 	public static String VerifyLoginFailText = "//form[@action='/login']//p";
 	public static String VerifyRegistrationFailText = "//form[@action='/signup']//p";
-	
-	//Links
-    
 
 	//Buttons
 	public static String SignUpButton = "//form[@action='/signup']//button[@type='submit']";
 	public static String LoginButton = "//form[@action='/login']//button[@type='submit']";
 	
-	
 	//FormFields
-    public static String LoginEmailFormField = "//form[@action='/login']//input[@type='email']";
-    public static String LoginPasswordFormField = "//form[@action='/login']//input[@type='password']";
-    public static String RegisterNameFormField = "//form[@action='/signup']//input[@type='text']";
-    public static String RegisterEmailFormField = "//form[@action='/signup']//input[@type='email']";
-	
-	//DropMenu
-
-
+        public static String LoginEmailFormField = "//form[@action='/login']//input[@type='email']";
+        public static String LoginPasswordFormField = "//form[@action='/login']//input[@type='password']";
+        public static String RegisterNameFormField = "//form[@action='/signup']//input[@type='text']";
+        public static String RegisterEmailFormField = "//form[@action='/signup']//input[@type='email']";
 }
