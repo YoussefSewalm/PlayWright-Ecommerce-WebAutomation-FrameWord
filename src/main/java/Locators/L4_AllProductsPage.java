a194eea063b09ee5474d259085780e4099e75eb8
YoussefSewalm <eng.yousefsewalem@gmail.com>
2025-01-12 01:17:37 +0200

    Update L4_AllProductsPage.java

--- src/main/java/Locators/L4_AllProductsPage.java
+++ src/main/java/Locators/L4_AllProductsPage.java
@@ -8,16 +8,12 @@ public class L4_AllProductsPage {
 	public static String ProductNamesText = "//div[@class='single-products']//descendant::p";
 	
 	//Links
-    public static String ViewCartLink = "//div[@class='modal-body']//descendant::a";
+       public static String ViewCartLink = "//div[@class='modal-body']//descendant::a";
 
 	//Buttons
-    public static String ContinueShoppingButton = "//div[@class='modal-content']//descendant::button";
-    public static String SearchButton = "//section[@id='advertisement']//div//button";
+       public static String ContinueShoppingButton = "//div[@class='modal-content']//descendant::button";
+       public static String SearchButton = "//section[@id='advertisement']//div//button";
     
 	//FormFields
-    public static String SearchProductFormField = "//section[@id='advertisement']//div//input";
-    
-    
-	//CheckBox
-
+       public static String SearchProductFormField = "//section[@id='advertisement']//div//input";
 }
