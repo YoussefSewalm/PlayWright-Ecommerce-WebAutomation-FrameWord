c3b59e60abb3e9fab880510a78c34358eb00f0e6
YoussefSewalm <eng.yousefsewalem@gmail.com>
2025-01-12 01:17:49 +0200

    Update L5_ProductDetailsPage.java

--- src/main/java/Locators/L5_ProductDetailsPage.java
+++ src/main/java/Locators/L5_ProductDetailsPage.java
@@ -5,7 +5,6 @@ public class L5_ProductDetailsPage {
 	//Buttons
 	public static String AddToCartFromProductDetailsButton = "button[type='button']";
 	
-	
 	//Texts
 	public static String VerifyProductNameText = "//div[@class='product-information']//h2";
 	public static String VerifyProductPriceText = "//div[@class='product-information']//descendant::span//span";
