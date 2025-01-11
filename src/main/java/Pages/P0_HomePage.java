0a94bccc306dbb1554e6c5cfc3986ebec69637f7
YoussefSewalm <eng.yousefsewalem@gmail.com>
2025-01-12 01:08:11 +0200

    Update P0_HomePage.java

--- src/main/java/Pages/P0_HomePage.java
+++ src/main/java/Pages/P0_HomePage.java
@@ -10,22 +10,13 @@ public class P0_HomePage {
 	public static Texts VerifyLoggedIn = new Texts(VerifyLoggedInText);
 	
 	//Links
-    public static Links HomePage = new Links(HomePageLink);
-    public static Links ProductsPage = new Links(ProductsPageLink);
-    public static Links CartPage = new Links(CartPageLink);
-    public static Links LoginSignUp = new Links(LoginSignUpLink);
-    public static Links ContactUs = new Links(ContactUsLink);
-    public static Links LogOut = new Links(LogOutLink);
-    public static Links DeleteAccount = new Links(DeleteAccountLink);
-	
-	//Buttons
-	
-	
-	//FormFields
-
-	
-	//DropMenu
-
+        public static Links HomePage = new Links(HomePageLink);
+        public static Links ProductsPage = new Links(ProductsPageLink);
+        public static Links CartPage = new Links(CartPageLink);
+        public static Links LoginSignUp = new Links(LoginSignUpLink);
+        public static Links ContactUs = new Links(ContactUsLink);
+        public static Links LogOut = new Links(LogOutLink);
+        public static Links DeleteAccount = new Links(DeleteAccountLink);
 	
 
 }
