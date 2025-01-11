4567eee1778f4ac96358fccb4ab349e49cf7d008
YoussefSewalm <eng.yousefsewalem@gmail.com>
2025-01-12 01:10:18 +0200

    Update P1_AccountPage.java

--- src/main/java/Pages/P1_AccountPage.java
+++ src/main/java/Pages/P1_AccountPage.java
@@ -11,19 +11,14 @@ public class P1_AccountPage {
 	    public static Texts VerifyLoginFail = new Texts(VerifyLoginFailText);
 	    public static Texts VerifyRegisterationFail = new Texts(VerifyRegistrationFailText);
 
-		//Links
-
-		//Buttons
-		public static Buttons SignUp = new Buttons(SignUpButton);
-		public static Buttons Login = new Buttons(LoginButton);
+	    //Buttons
+	    public static Buttons SignUp = new Buttons(SignUpButton);
+	    public static Buttons Login = new Buttons(LoginButton);
 		
-		//FormFields
-        public static FormFields LoginEmail = new FormFields(LoginEmailFormField);
-        public static FormFields LoginPassword = new FormFields(LoginPasswordFormField);
-        public static FormFields RegisterName = new FormFields(RegisterNameFormField);
-        public static FormFields RegisterEmail = new FormFields(RegisterEmailFormField);
+	    //FormFields
+            public static FormFields LoginEmail = new FormFields(LoginEmailFormField);
+            public static FormFields LoginPassword = new FormFields(LoginPasswordFormField);
+            public static FormFields RegisterName = new FormFields(RegisterNameFormField);
+            public static FormFields RegisterEmail = new FormFields(RegisterEmailFormField);
 
-		//DropMenu
-
-		
 }
